67d6567e71b1fb7cbaf163d52be3d93192662686
Michał Stolarczyk <cimlahcimlah@gmail.com>
2024-08-15 11:27:49 +0200

    Added `GameMenu` to main `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
+/** @format */
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GameSettings } from './types/GameSettings'
+import { GameMenu } from './components/GameMenu'
+
+// type FirstClickCoords = null | { x: number; y: number }
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [map, setMap] = useState<null | Cell[][]>(null)
+  const [didGameStart, setDidGameStart] = useState(false)
+  const [settings, setSettings] = useState<null | GameSettings>(null)
+
+  console.log(settings)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!didGameStart && (
+        <GameMenu
+          setDidGameStart={setDidGameStart}
+          setSettings={setSettings}
+        />
+      )}
     </>
   )
 }
